dc6370b716bf55de7e363143a8732645473a3395
Hussanar A <huss2996@global.chainsys.com>
2022-03-03 07:56:33 +0530

    js

---
+++ src/pageh.js
@@ -0,0 +1,5 @@
+const vGlobal = 'this is global'
+function scopefunction () {
+  alsoGlobal = 'this is also global variable'
+  vNotGlobal = 'this is private and scoped to the function'
+}
--- src/sampleb1.js
+++ src/sampleb1.js
@@ -1,5 +1,7 @@
+/* eslint-disable semi */
 const p1 = document.querySelector('div');
 
 console.log(p1);
 
+// eslint-disable-next-line eol-last
 console.log('Hello there Again');
